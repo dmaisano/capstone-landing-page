21c1be89fdba8f59a264fbcc46b18457db3980d6
Domenico Maisano <dev.dmaisano@gmail.com>
2019-10-14 22:23:13 -0400

    working on splash page

--- soho-app/src/app/app-routing.module.ts
+++ soho-app/src/app/app-routing.module.ts
@@ -1,11 +1,11 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import { NgModule } from "@angular/core";
+import { Routes, RouterModule } from "@angular/router";
+import { LandingPageComponent } from "./landing-page/landing-page.component";
 
-
-const routes: Routes = [];
+const routes: Routes = [{ path: "", component: LandingPageComponent }];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  imports: [RouterModule.forRoot(routes, { useHash: true })],
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
