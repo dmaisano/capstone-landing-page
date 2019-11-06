2927efb971dca4408268bc8d1ef9b9c65d39f4f3
Domenico Maisano <dev.dmaisano@gmail.com>
2019-11-05 22:28:15 -0500

    added stripe integration

--- soho-app/src/app/landing-page/landing-page.component.ts
+++ soho-app/src/app/landing-page/landing-page.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from "@angular/core";
+import { environment } from "src/environments/environment";
+import Swal from "sweetalert2";
 
 @Component({
   selector: "app-landing-page",
@@ -6,7 +8,41 @@ import { Component, OnInit } from "@angular/core";
   styleUrls: ["./landing-page.component.scss"],
 })
 export class LandingPageComponent implements OnInit {
+  handler: any;
+
   constructor() {}
 
-  ngOnInit() {}
+  ngOnInit() {
+    this.handler = Stripe(environment.stripe.apiKey);
+  }
+
+  async checkout(type: "monthly" | "yearly") {
+    const plan =
+      type === "monthly"
+        ? environment.stripe.products.monthly
+        : environment.stripe.products.yearly;
+
+    try {
+      const result = await this.handler.redirectToCheckout({
+        items: [{ plan, quantity: 1 }],
+
+        successUrl: environment.stripe.redirectUrl,
+        cancelUrl: environment.stripe.redirectUrl,
+      });
+
+      if (result.error) {
+        Swal.fire({
+          icon: "error",
+          title: "Error",
+          text: result.error.message,
+        });
+      }
+    } catch (error) {
+      Swal.fire({
+        icon: "error",
+        title: "Error",
+        text: error,
+      });
+    }
+  }
 }
--- soho-app/src/environments/environment.ts
+++ soho-app/src/environments/environment.ts
@@ -3,7 +3,15 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  stripe: {
+    apiKey: "pk_test_hjBfW7Lz04N0C123vP5vQS5r00nNzLukTx",
+    products: {
+      monthly: "plan_FwuGHRnyYF2M4J",
+      yearly: "plan_FwuBzBQuOLw4ye",
+    },
+    redirectUrl: "https://sohomuse-landing-page.netlify.com",
+  },
 };
 
 /*
