e8c8007768fd562a2746abe7a92085d4264a93e3
Domenico Maisano <dev.dmaisano@gmail.com>
2019-11-05 22:39:28 -0500

    added env config

--- soho-app/src/environments/environment.prod.ts
+++ soho-app/src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: false,
+  stripe: {
+    apiKey: "pk_test_hjBfW7Lz04N0C123vP5vQS5r00nNzLukTx",
+    products: {
+      monthly: "plan_FwuGHRnyYF2M4J",
+      yearly: "plan_FwuBzBQuOLw4ye",
+    },
+    redirectUrl: "https://sohomuse-landing-page.netlify.com",
+  },
 };
